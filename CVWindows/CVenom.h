ad8bded8bb753bacf295eb49dfc794255ce61330
Mahmoud Osman (MLT) <60033959+Mahmoud7Osman@users.noreply.github.com>
2022-02-23 16:31:36 +0200

    Add files via upload

--- CVWindows/CVenom.h
+++ CVWindows/CVenom.h
@@ -21,29 +21,42 @@
 
 // Accessing Some Functions From Different Namespaces.
 using		std::string;
-using       std::remove;
+using       	std::remove;
 
 // Custom Data Types And Typedefs
 typedef void (*function)(int, int);
 struct c_malware_stat_t{
 	char	name[256];
-	char*	path;
 
 	size_t  size;
 };
 
 // Global Data For Some CVenom's Functionalities.
-char**						_argv__;
-int							_argc__;
+char**				_argv__;
+int				_argc__;
+
 struct c_malware_stat_t 	Current;
+struct c_malware_stat_t*	MALWARE;
 
 // Some Functions For Initializing The Library.
 void cvinit(int argc, char** argv){
+	HANDLE fh;
 	_argv__=argv;
 	_argc__=argc;
-	
+	Malware=&Current;
+
+	fh=CreateFile(argv[0], GENERIC_READ, 0x00, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+
+	Current.size=GetFileSize(fh, NULL);
+
+	strncpy(Current.name, argv[0], 256);
+
+	CloseHandle(fh);
+
 	return;
-	
+
+
+
 }
 
 void cvexit(int x){
---
+++ CVWindows/RuntimeTools.h
@@ -0,0 +1,4 @@
+class RuntimeTools{
+	private:
+	public:
+}

